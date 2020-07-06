62a982c6bafb5154a3a8f0945a2a13e435aad3aa
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2020-07-07 05:29:18 +0530

    Add ethereum on-chain tx & wallet backends
    These backends provide the functionality to interact with an ethereum
    network and work with ethereum wallets.
    
    The ethereum package is structured so that it can compiled as a
    plugin and dynamically linked during run time without importing any
    code from ethereum related libraries (in go-ethereum, that is
    licensed under LGPL and in go-perun/backend/ethereum that imports
    go-ethereum). However, it is currently used by importing
    (static linking) for ease of use.
    
    For detailed information see package docs.
    
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

---
+++ blockchain/ethereum/ethereumtest/chain.go
@@ -0,0 +1,88 @@
+// Copyright (c) 2020 - for information on the respective copyright owner
+// see the NOTICE file and/or the repository at
+// https://github.com/direct-state-transfer/dst-go
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ethereumtest
+
+import (
+	"context"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/direct-state-transfer/dst-go/blockchain/ethereum/internal"
+
+	ethchannel "perun.network/go-perun/backend/ethereum/channel"
+	ethchanneltest "perun.network/go-perun/backend/ethereum/channel/test"
+	ethwallet "perun.network/go-perun/backend/ethereum/wallet"
+	"perun.network/go-perun/wallet"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/stretchr/testify/require"
+
+	"github.com/direct-state-transfer/dst-go"
+)
+
+// DefaultTxTimeout is the default transaction timeout for simulated backend.
+const DefaultTxTimeout = 5 * time.Second
+
+// ChainBackendSetup is a test setup that uses a simulated blockchain backend (for details on this backend,
+// see go-ethereum) with required contracts deployed on it and a UserSetup.
+type ChainBackendSetup struct {
+	*WalletSetup
+	ChainBackend       dst.ChainBackend
+	AdjAddr, AssetAddr wallet.Address
+}
+
+// NewChainBackendSetup returns a simulated contract backend  with assetHolder, adjudicator contracts deployed.
+// It also generate the given number of accounts and funds them each with 10 ether.
+// and returns a test ChainBackend using the given randomness.
+func NewChainBackendSetup(t *testing.T, rng *rand.Rand, cntFundedAccs uint) (_ *ChainBackendSetup) {
+	// userSetup := NewUserSetup(t, rng)
+	walletSetup := NewWalletSetup(t, rng, cntFundedAccs)
+
+	cbEth := newSimContractBackend(walletSetup.Accs, walletSetup.Keystore)
+	cb := &internal.ChainBackend{Cb: &cbEth}
+
+	ctx, cancel := context.WithTimeout(context.Background(), DefaultTxTimeout)
+	defer cancel()
+	adjudicator, err := ethchannel.DeployAdjudicator(ctx, cbEth)
+	require.NoError(t, err)
+	asset, err := ethchannel.DeployETHAssetholder(ctx, cbEth, adjudicator)
+	require.NoError(t, err)
+
+	// No cleanup required.
+	return &ChainBackendSetup{
+		WalletSetup:  walletSetup,
+		ChainBackend: cb,
+		AdjAddr:      ethwallet.AsWalletAddr(adjudicator),
+		AssetAddr:    ethwallet.AsWalletAddr(asset),
+	}
+}
+
+// newSimContractBackend sets up a simulated contract backend with the first entry (index 0) in accs
+// as the user accounts. All accounts are funded with 10 ethers.
+func newSimContractBackend(accs []wallet.Account, ks *keystore.KeyStore) ethchannel.ContractBackend {
+	simBackend := ethchanneltest.NewSimulatedBackend()
+	ctx, cancel := context.WithTimeout(context.Background(), DefaultTxTimeout)
+	defer cancel()
+	for i := range accs {
+		simBackend.FundAddress(ctx, ethwallet.AsEthAddr(accs[i].Address()))
+	}
+
+	onChainAcc := &accs[0].(*ethwallet.Account).Account
+	contractBackend := ethchannel.NewContractBackend(simBackend, ks, onChainAcc)
+	return contractBackend
+}
---
+++ blockchain/ethereum/internal/chain.go
@@ -0,0 +1,89 @@
+// Copyright (c) 2020 - for information on the respective copyright owner
+// see the NOTICE file and/or the repository at
+// https://github.com/direct-state-transfer/dst-go
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"context"
+	"time"
+
+	"github.com/pkg/errors"
+
+	ethchannel "perun.network/go-perun/backend/ethereum/channel"
+	ethwallet "perun.network/go-perun/backend/ethereum/wallet"
+	"perun.network/go-perun/channel"
+	"perun.network/go-perun/wallet"
+)
+
+// ChainBackend provides ethereum specific contract backend functionality.
+type ChainBackend struct {
+	// Cb is the instance of contract backend that will be used for all on-chain communications.
+	Cb *ethchannel.ContractBackend
+}
+
+// NewFunder initializes and returns an instance of ethereum funder.
+func (cb *ChainBackend) NewFunder(assetAddr wallet.Address) channel.Funder {
+	return newFunder(*cb.Cb, assetAddr)
+}
+
+// NewAdjudicator initializes and returns an instance of ethereum adjudicator.
+func (cb *ChainBackend) NewAdjudicator(adjAddr, receiverAddr wallet.Address) channel.Adjudicator {
+	return newAdjudicator(*cb.Cb, adjAddr, receiverAddr)
+}
+
+// ValidateContracts validates the integrity of given adjudicator and asset holder contracts.
+func (cb *ChainBackend) ValidateContracts(adjAddr, assetAddr wallet.Address) error {
+	return validateContracts(*cb.Cb, adjAddr, assetAddr)
+}
+
+// DeployAdjudicator deploys the adjudicator contract.
+func (cb *ChainBackend) DeployAdjudicator(ctx context.Context) (wallet.Address, error) {
+	return deployAdjudicator(ctx, *cb.Cb)
+}
+
+// DeployAsset deploys the asset holder contract, setting the adjudicator address to given value.
+func (cb *ChainBackend) DeployAsset(ctx context.Context, adjAddr wallet.Address) (wallet.Address, error) {
+	return deployAsset(ctx, *cb.Cb, adjAddr)
+}
+
+func newFunder(cb ethchannel.ContractBackend, assetAddr wallet.Address) channel.Funder {
+	return ethchannel.NewETHFunder(cb, ethwallet.AsEthAddr(assetAddr))
+}
+
+func newAdjudicator(cb ethchannel.ContractBackend, adjAddr, receiverAddr wallet.Address) channel.Adjudicator {
+	return ethchannel.NewAdjudicator(cb, ethwallet.AsEthAddr(adjAddr), ethwallet.AsEthAddr(receiverAddr))
+}
+
+func validateContracts(cb ethchannel.ContractBackend, adjAddr, assetAddr wallet.Address) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	// Integrity of Adjudicator is implicitly done during validation of asset holder contract.
+	err := ethchannel.ValidateAssetHolderETH(ctx, cb, ethwallet.AsEthAddr(assetAddr), ethwallet.AsEthAddr(adjAddr))
+	if ethchannel.IsContractBytecodeError(err) {
+		return errors.Wrap(err, "invalid contracts at given address")
+	}
+	return errors.Wrap(err, "validating contracts")
+}
+
+func deployAdjudicator(ctx context.Context, cb ethchannel.ContractBackend) (wallet.Address, error) {
+	addr, err := ethchannel.DeployAdjudicator(ctx, cb)
+	return ethwallet.AsWalletAddr(addr), err
+}
+
+func deployAsset(ctx context.Context, cb ethchannel.ContractBackend, adjAddr wallet.Address) (wallet.Address, error) {
+	addr, err := ethchannel.DeployETHAssetholder(ctx, cb, ethwallet.AsEthAddr(adjAddr))
+	return ethwallet.AsWalletAddr(addr), err
+}
