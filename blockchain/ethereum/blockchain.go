307bd635da3dd164af9a78f23c1a90b229f40694
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2020-07-12 00:49:01 +0530

    to ethereum backend
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- blockchain/ethereum/blockchain.go
+++ blockchain/ethereum/blockchain.go
@@ -17,29 +17,37 @@
 package ethereum
 
 import (
+	"context"
+	"time"
+
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/pkg/errors"
 	ethchannel "perun.network/go-perun/backend/ethereum/channel"
-	"perun.network/go-perun/wallet"
+	ethwallet "perun.network/go-perun/backend/ethereum/wallet"
 
 	"github.com/direct-state-transfer/dst-go"
 	implementation "github.com/direct-state-transfer/dst-go/blockchain/ethereum/internal/implementation"
 )
 
-// NewOnChainTxBackend initializes a connection to blockchain node and
-// sets up a wallet for funding onchain transactions.
+// NewOnChainTxBackend initializes a connection to blockchain node and sets up a wallet for funding onchain transactions.
 //
 // It returns an interface to enable this function to be loaded as a symbol using types defined in dst-go alone.
-func NewOnChainTxBackend(url, userKeystorePath string, userAddr wallet.Address) (dst.OnChainTxBackend, error) {
-	ethereumBackend, err := ethclient.Dial(url)
+func NewOnChainTxBackend(url string, timeout time.Duration, cred dst.Credential) (dst.OnChainTxBackend, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	ethereumBackend, err := ethclient.DialContext(ctx, url)
 	if err != nil {
 		return nil, errors.Wrap(err, "connecting to ethereum node")
 	}
-	ks := keystore.NewKeyStore(userKeystorePath, standardScryptN, standardScryptP)
-	acc := &accounts.Account{Address: common.BytesToAddress(userAddr.Bytes())}
-	cb := ethchannel.NewContractBackend(ethereumBackend, ks, acc)
+
+	ks := keystore.NewKeyStore(cred.Keystore, standardScryptN, standardScryptP)
+	acc := accounts.Account{Address: ethwallet.AsEthAddr(cred.Addr)}
+	err = ks.Unlock(acc, cred.Password)
+	if err != nil {
+		return nil, errors.Wrap(err, "unlocking on-chain keystore")
+	}
+	cb := ethchannel.NewContractBackend(ethereumBackend, ks, &acc)
 	return &implementation.OnChainTxBackend{Cb: &cb}, nil
 }
