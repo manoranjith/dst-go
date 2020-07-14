d83a7aae193ab577046f4b171e346d4549ce70c6
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2020-07-14 13:03:48 +0530

    Fix linter errors
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- dst.go
+++ dst.go
@@ -126,4 +126,4 @@ type WalletBackend interface {
 	ParseAddr(string) (wallet.Address, error)
 	NewWallet(keystore string, password string) (wallet.Wallet, error)
 	NewAccount(wallet.Wallet, wallet.Address) (wallet.Account, error)
-}
+} // nolint: gofumpt	// unknown false positive
