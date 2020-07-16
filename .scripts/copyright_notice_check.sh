5bf329dacf7107efa60cf35f60d804f794d81af8
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2020-07-16 13:47:47 +0530

    improve print op
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- .scripts/copyright_notice_check.sh
+++ .scripts/copyright_notice_check.sh
@@ -9,7 +9,7 @@ function check_copyright_notice() {
   f=$1
   diff_output=$(diff --color=always <(sed -ne "${start_line},${end_line}p" $f | \
   sed "s/20\(19\|2[0-9]\)/20XX/") $template)
-  [ $? -ne 0 ] && echo -e "\033[1m\n$f\033[0m" && echo "$diff_output"
+  [ $? -ne 0 ] && echo -e "\033[1m\nIn file $f\033[0m" && echo "$diff_output"
 }
 
 exit_status=0
@@ -21,5 +21,5 @@ for f in $(find . -name "*.go"); do
   [ $? -ne 0 ] && exit_status=1
 done
 [ $exit_status -ne 0 ] && echo -e "\e[30;1;47m\n\nTo fix,\
- replace the red lines marked \"\<\" with (green lines marked \"\>\". \033[0m"
+ replace the red lines marked \"<\" with green lines marked \">\". \033[0m"
 exit $exit_status
