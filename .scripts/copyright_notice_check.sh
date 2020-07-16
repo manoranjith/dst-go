b1d79897838ef01707c2758ff69385288a7257ee
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2020-07-16 17:25:37 +0530

    script final
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- .scripts/copyright_notice_check.sh
+++ .scripts/copyright_notice_check.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Formating directives for printing text.
 bold=`tput bold`
 bold_highlight=`tput setab 120 bold`
 reset=`tput sgr0`
+
+exit_status=0 #Set this to 1, when first error is detected.
 
 template="$(dirname $0)/copyright_notice_template.txt"
 n=$(wc -l $template | awk '{ print $1 }')
@@ -13,17 +16,16 @@ function check_copyright_notice() {
   f=$1
   diff_output=$(diff --color=always <(sed -ne "${start_line},${end_line}p" $f | \
   sed "s/20\(19\|2[0-9]\)/20XX/") $template)
-  [ $? -ne 0 ] && echo -e "${bold}\nIn file $f\n$diff_output"
+  [ $? -ne 0 ] && exit_status=1 && echo -e "${bold}\nIn file $f\n$diff_output"
 }
 
-exit_status=0
 for f in $(find . -name "*.go"); do
   # Skip generated files, Identified by DO NOT EDIT phrase in line 1.
   if ! sed -ne '1,1p' $f | grep "DO NOT EDIT." -q; then
     check_copyright_notice $f
   fi
-  [ $? -ne 0 ] && exit_status=1
 done
+
 [ $exit_status -ne 0 ] && echo -e "$bold_highlight\n\nHints to fix:$reset\n
 1. The actual text in the file is marked red and the expected content
    is marked green.
