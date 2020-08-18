c4d9b3b92c0724a2f15214d0695293e09acc98d0
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2020-08-19 00:12:01 +0530

    Improve setinal error defs
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- errors.go
+++ errors.go
@@ -1,36 +1,49 @@
 package perun
 
-import (
-	"fmt"
-)
+import "errors"
+
+// APIError represents the errors that will communicated via the user API.
+type APIError string
+
+func (e APIError) Error() string {
+	return string(e)
+}
+
+// GetAPIError returns the APIError contained in err if err is an APIError.
+// If not, it returns ErrInternalServer API error.
+func GetAPIError(err error) APIError {
+	var apiErr APIError
+	if !errors.As(err, &apiErr) {
+		return ErrInternalServer
+	}
+	return apiErr
+}
 
 // Setinal Error values that are relevant for the end user of the node.
 var (
-	ErrUnknownSessionID  = fmt.Errorf("No session corresponding to the specified ID.")
-	ErrUnknownProposalID = fmt.Errorf("No channel proposal corresponding to the specified ID.")
-	ErrUnknownChannelID  = fmt.Errorf("No channel corresponding to the specified ID.")
-	ErrUnknownAlias      = fmt.Errorf("No peer corresponding to the specified ID was found in contacts.")
-	ErrUnknownUpdateID   = fmt.Errorf("No response was expected for the given channel update ID")
+	ErrUnknownSessionID  = APIError("No session corresponding to the specified ID.")
+	ErrUnknownProposalID = APIError("No channel proposal corresponding to the specified ID.")
+	ErrUnknownChannelID  = APIError("No channel corresponding to the specified ID.")
+	ErrUnknownAlias      = APIError("No peer corresponding to the specified ID was found in contacts.")
+	ErrUnknownUpdateID   = APIError("No response was expected for the given channel update ID")
 
-	ErrUnsupportedCurrency     = fmt.Errorf("Currency not supported by this node instance.")
-	ErrUnsupportedContactsType = fmt.Errorf("Contacts type not supported by this node instance.")
-	ErrUnsupportedCommType     = fmt.Errorf("Communication protocol not supported by this node instance.")
+	ErrUnsupportedCurrency     = APIError("Currency not supported by this node instance.")
+	ErrUnsupportedContactsType = APIError("Contacts type not supported by this node instance.")
+	ErrUnsupportedCommType     = APIError("Communication protocol not supported by this node instance.")
 
-	ErrInsufficientBal     = fmt.Errorf("Insufficient balance in sender account.")
-	ErrInvalidAmount       = fmt.Errorf("Invalid amount string.")
-	ErrInvalidConfig       = fmt.Errorf("Invalid configuration detected.")
-	ErrInvalidOffChainAddr = fmt.Errorf("Invalid off-chain address string.")
+	ErrInsufficientBal     = APIError("Insufficient balance in sender account.")
+	ErrInvalidAmount       = APIError("Invalid amount string.")
+	ErrInvalidConfig       = APIError("Invalid configuration detected.")
+	ErrInvalidOffChainAddr = APIError("Invalid off-chain address string.")
 
-	ErrNoActiveSub      = fmt.Errorf("No active subscription was found.")
-	ErrSubAlreadyExists = fmt.Errorf("A subscription for this context already exists.")
+	ErrNoActiveSub      = APIError("No active subscription was found.")
+	ErrSubAlreadyExists = APIError("A subscription for this context already exists.")
 
-	ErrPeerAliasInUse = fmt.Errorf("Alias already used by another peer in the contacts.")
-	ErrPeerExists     = fmt.Errorf("Peer already available in the contacts provider.")
+	ErrPeerAliasInUse     = APIError("Alias already used by another peer in the contacts.")
+	ErrPeerExists         = APIError("Peer already available in the contacts provider.")
+	ErrRespTimeoutExpired = APIError("Response to the notification was sent after the timeout has expired.")
+	ErrPeerRejected       = APIError("The request was rejected by the peer.")
 
-	// ErrPeerNotResponding   = "Peer did not respond within expected timeout."
-	// ErrRespTimeoutExpired  = "Response to the notification was sent after the timeout has expired."
-	ErrPeerRejected = fmt.Errorf("The request was rejected by the peer.")
-
-	ErrUnclosedPayCh  = fmt.Errorf("Session cannot be closed (without force option as there are unclosed channels.")
-	ErrInternalServer = fmt.Errorf("Internal Server Error")
+	ErrUnclosedCh     = APIError("Session cannot be closed (without force option as there are unclosed channels.")
+	ErrInternalServer = APIError("Internal Server Error")
 )
